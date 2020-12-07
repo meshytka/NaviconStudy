e53ee1920b8422e9eafae8389d28aca03c09df2c
Adasda <asdasdas>
2020-12-07 16:57:31 +0300

    Plugins: Add PostInvoiceCreateAsynk

---
+++ CRM/CRM.Plugins/nav_invoice/PostInvoiceCreateAsynk.cs
@@ -0,0 +1,40 @@
+﻿using CRM.Plugins.nav_invoice.Handlers;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Plugins.nav_invoice
+{
+    public sealed class PostInvoiceCreateAsynk : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var traceService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            traceService.Trace("Info about trace");
+
+            var pluginContext = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var targetInvoice = (Entity)pluginContext.InputParameters["Target"];
+
+            traceService.Trace("Name " + targetInvoice.LogicalName);
+
+            var serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            var service = serviceFactory.CreateOrganizationService(Guid.Empty);
+
+            try
+            {
+                InvoiceService invoiceService = new InvoiceService(service, traceService);
+
+                invoiceService.PostInvoiceRecalculating(targetInvoice);
+            }
+            catch (Exception exc)
+            {
+                traceService.Trace(exc.ToString());
+
+                throw new InvalidPluginExecutionException(exc.Message);
+            }
+        }
+    }
+}
